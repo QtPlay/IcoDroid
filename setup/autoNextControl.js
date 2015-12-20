0a0495b9eed6b1655b7c852965aa94a6b4bc0aa8
Skycoder42 <Skycoder42@users.noreply.github.com>
2015-12-20 13:01:54 +0100

    config scripts updated

--- setup/autoNextControl.js
+++ setup/autoNextControl.js
@@ -5,25 +5,31 @@ function Controller()
 
 Controller.prototype.IntroductionPageCallback = function()
 {
-    if (installer.isUpdater()) {
-        // Get the current wizard page
-        var widget = gui.currentPageWidget();
-        if (widget !== null) {
-            // Don't show buttons because we just want to uninstall the software
-            widget.findChild("PackageManagerRadioButton").checked = false;
-            widget.findChild("UpdaterRadioButton").checked = true;
-            widget.findChild("UninstallerRadioButton").checked = false;
-            gui.clickButton(buttons.NextButton);
+    var widget = gui.currentPageWidget();
+    if(installer.isOfflineOnly()) {
+        if (!installer.isInstaller()) {
+            if (widget !== null) {
+                widget.findChild("PackageManagerRadioButton").visible = false;
+                widget.findChild("UpdaterRadioButton").visible = false;
+                widget.findChild("UninstallerRadioButton").checked = true;
+                gui.clickButton(buttons.NextButton);
+            }
         }
-    } else if(installer.isPackageManager()) {
-        // Get the current wizard page
-        var widget = gui.currentPageWidget();
-        if (widget !== null) {
-            // Don't show buttons because we just want to uninstall the software
-            widget.findChild("PackageManagerRadioButton").checked = true;
-            widget.findChild("UpdaterRadioButton").checked = false;
-            widget.findChild("UninstallerRadioButton").checked = false;
-            gui.clickButton(buttons.NextButton);
+    } else {
+        if (installer.isUpdater()) {
+            if (widget !== null) {
+                widget.findChild("PackageManagerRadioButton").checked = false;
+                widget.findChild("UpdaterRadioButton").checked = true;
+                widget.findChild("UninstallerRadioButton").checked = false;
+                gui.clickButton(buttons.NextButton);
+            }
+        } else if(installer.isPackageManager()) {
+            if (widget !== null) {
+                widget.findChild("PackageManagerRadioButton").checked = true;
+                widget.findChild("UpdaterRadioButton").checked = false;
+                widget.findChild("UninstallerRadioButton").checked = false;
+                gui.clickButton(buttons.NextButton);
+            }
         }
     }
 }
