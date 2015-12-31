d9dcaf2d0728924a83947ec9b0205a8890866952
Skycoder42 <Skycoder42@users.noreply.github.com>
2015-12-31 13:52:15 +0100

    install rdy linux (and all)

--- build_scripts/linux/install.sh
+++ build_scripts/linux/install.sh
@@ -12,9 +12,10 @@ cd ./install
 
 #config
 cp $2/build_scripts/linux/config.xml ./config/
+cp $2/setup/autoNextControl.js ./config/
 
 #package
-cp $2/build_scripts/linux/package.xml ./packages/com.SkycoderSoft.IcoDroid/meta/
+cp $2/setup/package.xml ./packages/com.SkycoderSoft.IcoDroid/meta/
 cp $2/setup/install.js ./packages/com.SkycoderSoft.IcoDroid/meta/
 cp $2/setup/shortcutPage.ui ./packages/com.SkycoderSoft.IcoDroid/meta/
 cp $2/LICENSE ./packages/com.SkycoderSoft.IcoDroid/meta/
@@ -22,4 +23,4 @@ cp $2/icons/main.png ./packages/com.SkycoderSoft.IcoDroid/data/
 
 mkdir IcoDroid
 $1/bin/repogen -p ./packages ./IcoDroid/linux_x64
-$1/bin/binarycreator -n -c ./config/config.xml -p ./packages IcoDroid_1.0.0_setup.run
+$1/bin/binarycreator -n -c ./config/config.xml -p ./packages IcoDroid_1.1.0_setup.run
