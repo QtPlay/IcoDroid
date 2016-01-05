f3645dfce41b300c7832029ac5b15b45c147c448
Felix Barz <Skycoder42@users.noreply.github.com>
2016-01-05 15:50:25 +0100

    Update regSetUninst.bat

--- build_scripts/win/regSetUninst.bat
+++ build_scripts/win/regSetUninst.bat
@@ -2,5 +2,10 @@
 : %2: maintanancetool path
 : %3: base_key
 : %4: node
+: %5: 0: offline; 1: online
 @echo off
-reg add "%~3\SOFTWARE\%~4\Windows\CurrentVersion\Uninstall\%~1" /f /v "UninstallString" /t REG_SZ /d "%~2 uninstallOnly=1"
+IF "%5" == "0" (
+	reg add "%~3\SOFTWARE\%~4\Windows\CurrentVersion\Uninstall\%~1" /f /v "NoModify" /t REG_DWORD /d 1
+) ELSE (
+	reg add "%~3\SOFTWARE\%~4\Windows\CurrentVersion\Uninstall\%~1" /f /v "UninstallString" /t REG_SZ /d "%~2 uninstallOnly=1"
+)
